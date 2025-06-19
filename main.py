1c63b778581c362e503bacaeb95e27481addc811
prokenz101 <kenz.v@outlook.com>
2025-06-19 18:35:14 +0400

    moved functions outside main try block

--- main.py
+++ main.py
@@ -14,7 +14,63 @@ def rgb_to_curses_color(r, g, b):
     return int(r * 1000 / 255), int(g * 1000 / 255), int(b * 1000 / 255)
 
 
+def exit_seal():
+    #* Exit the program and restore terminal settings
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+    exit()
+
+
+def main(stdscr):
+    #* Set up color pairs for colorful text
+    curses.start_color()
+
+    if curses.can_change_color():
+        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+
         curses.init_color(11, *rgb_to_curses_color(125, 229, 255))  #* teal
+        curses.init_pair(4, 11, curses.COLOR_BLACK)
+    else:
+        stdscr.clear()
+        exception_handler(
+            message="\033[96mYour terminal does not support colored text.\033[0m"
+        )
+
+    #* Start SEAL
+
+    try:
+        from scripts.user import choose_username
+        from scripts.encryption import encrypt, decrypt
+        from scripts.menu import welcome
+
+        welcome(stdscr)
+
+        username, password = choose_username(stdscr)
+        encrypt(
+            username,
+            password,
+            [
+                ["Outlook", "john@outlook.com", "InD9RQSVF0ZJ3up6", "azure"],
+                ["Google", "john@gmail.com", "FtF9VDFnxMMgzVlE", "green"],
+                ["Steam", "john@gmail.com", "lC3NdFOXXG9a9FHe", "blue"],
+            ],
+        )
+        decrypt(username, password)
+
+    except ModuleNotFoundError as e:
+        missing_file = str(e).split("'")[1].split(".")[1]
+        exception_handler(
+            message=f"\033[96mMissing required file '\033[92mscripts/{missing_file}.py\033[96m'. Please restore or re-download it.\033[0m",
+            Exception=e,
+        )
+    except Exception as e:
+        exception_handler(Exception=e)
+
+
 try:
     import curses
     import cryptography
@@ -23,59 +79,6 @@ try:
     curses.noecho()  #* Hides user inputs
     curses.cbreak()  #* React to keys instantly
     stdscr.keypad(True)  #* Enable the keypad keys
-
-    def exit_seal():
-        #* Exit the program and restore terminal settings
-        curses.nocbreak()
-        stdscr.keypad(False)
-        curses.echo()
-        curses.endwin()
-        exit()
-
-    def main(stdscr):
-        #* Set up color pairs for colorful text
-        curses.start_color()
-
-        if curses.can_change_color():
-            curses.init_color(10, 1000, 647, 0)  #* orange
-            curses.init_pair(1, 10, curses.COLOR_BLACK)
-
-            curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-            curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
-
-            curses.init_color(11, 0, 686, 800)  #* teal
-            curses.init_pair(4, 11, curses.COLOR_BLACK)
-        else:
-            stdscr.clear()
-            exception_handler(
-                message="\033[96mYour terminal does not support colored text.\033[0m"
-            )
-
-        #* Start SEAL
-
-        try:
-            from scripts.user import choose_username
-            from scripts.encryption import encrypt, decrypt
-
-            username, password = choose_username(stdscr)
-            encrypt(
-                username,
-                password,
-                [
-                    ["Outlook", "john@outlook.com", "InD9RQSVF0ZJ3up6", "azure"],
-                    ["Google", "john@gmail.com", "FtF9VDFnxMMgzVlE", "green"],
-                    ["Steam", "john@gmail.com", "lC3NdFOXXG9a9FHe", "blue"],
-                ],
-            )
-            decrypt(username, password)
-
-        except ModuleNotFoundError as e:
-            exception_handler(
-                message="\033[96mMissing required file '\033[92mmaster.py\033[96m'. Please restore or re-download it.\033[0m",
-                Exception=e,
-            )
-        except Exception as e:
-            exception_handler(Exception=e)
 
     curses.wrapper(main)
     exit_seal()
