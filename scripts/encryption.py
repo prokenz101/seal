b6116d6bcac8c1978ed4d49896ada756d403b96b
prokenz101 <kenz.v@outlook.com>
2025-07-06 18:07:54 +0400

    changes
    allowed for more direct encryption and decryption for strings and bulk data using their dedicated functions

--- scripts/encryption.py
+++ scripts/encryption.py
@@ -1,4 +1,3 @@
-import csv
 from hashlib import sha256
 from base64 import urlsafe_b64encode
 from cryptography.fernet import Fernet
@@ -27,33 +26,39 @@ def get_fernet_key(username_hash: str, master_password: str) -> Fernet:
     return fernet
 
 
-def encrypt(username: str, master_password: str, data: list) -> None:
-    encrypted_data = []
+def encrypt(username: str, master_password: str, data: str) -> str:
     username_hash = sha256(username.encode()).hexdigest()
     fernet = get_fernet_key(username_hash, master_password)
+    encrypted_data = fernet.encrypt(data.encode()).decode()
+    return encrypted_data
+
+
+def encrypt_data(username: str, master_password: str, data: list) -> list:
+    encrypted_data = []
 
     for record in data:
-        encrypted_record = [fernet.encrypt(field.encode()).decode() for field in record]
+        encrypted_record = [
+            encrypt(username, master_password, field) for field in record
+        ]
         encrypted_data.append(encrypted_record)
 
-    with open(f"encrypted_{username_hash}.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(encrypted_data)
+    return encrypted_data
 
 
-def decrypt(username: str, master_password: str):
+def decrypt(username: str, master_password: str, data: str) -> str:
+    username_hash = sha256(username.encode()).hexdigest()
+    fernet = get_fernet_key(username_hash, master_password)
+    decrypted_data = fernet.decrypt(data.encode()).decode()
+    return decrypted_data
+
+
+def decrypt_data(username: str, master_password: str, data: list) -> list:
     decrypted_data = []
     username_hash = sha256(username.encode()).hexdigest()
     fernet = get_fernet_key(username_hash, master_password)
 
-    with open(f"encrypted_{username_hash}.csv", "r") as f:
-        reader = csv.reader(f)
-        for record in reader:
-            decrypted_record = [
-                fernet.decrypt(field.encode()).decode() for field in record
-            ]
-            decrypted_data.append(decrypted_record)
+    for record in data:
+        decrypted_record = [fernet.decrypt(field.encode()).decode() for field in record]
+        decrypted_data.append(decrypted_record)
 
-    with open(f"decrypted_{username_hash}.csv", "w", newline="") as f_out:
-        writer = csv.writer(f_out)
-        writer.writerows(decrypted_data)
+    return decrypted_data
