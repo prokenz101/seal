25331dd3c39d48eebba57c49b0725ea3bbd2757b
prokenz101 <kenz.v@outlook.com>
2025-07-08 19:02:15 +0400

    added experimental button-style layout

--- scripts/menu.py
+++ scripts/menu.py
@@ -10,54 +10,46 @@ def first_time_launch(stdscr):
     prompt = ""
 
     while True:
-        stdscr.addstr(0, 11, "Welcome to", curses.A_BOLD)
-        stdscr.move(1, 0)
+        addstr(stdscr, 0, 11, "Welcome to", curses.A_BOLD)
+        move(stdscr, 1, 0)
         stdscr.clrtoeol()
-        stdscr.addstr(2, 0, "███████╗███████╗ █████╗ ██╗     ", curses.color_pair(4))
-        stdscr.addstr(3, 0, "██╔════╝██╔════╝██╔══██╗██║     ", curses.color_pair(4))
-        stdscr.addstr(4, 0, "███████╗█████╗  ███████║██║     ", curses.color_pair(4))
-        stdscr.addstr(5, 0, "╚════██║██╔══╝  ██╔══██║██║     ", curses.color_pair(4))
-        stdscr.addstr(6, 0, "███████║███████╗██║  ██║███████╗", curses.color_pair(4))
-        stdscr.addstr(7, 0, "╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝", curses.color_pair(4))
-        stdscr.move(8, 0)
+        addstr(stdscr, 2, 0, "███████╗███████╗ █████╗ ██╗     ", curses.color_pair(4))
+        addstr(stdscr, 3, 0, "██╔════╝██╔════╝██╔══██╗██║     ", curses.color_pair(4))
+        addstr(stdscr, 4, 0, "███████╗█████╗  ███████║██║     ", curses.color_pair(4))
+        addstr(stdscr, 5, 0, "╚════██║██╔══╝  ██╔══██║██║     ", curses.color_pair(4))
+        addstr(stdscr, 6, 0, "███████║███████╗██║  ██║███████╗", curses.color_pair(4))
+        addstr(stdscr, 7, 0, "╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝", curses.color_pair(4))
+        move(stdscr, 8, 0)
         stdscr.clrtoeol()
-        stdscr.move(9, 0)
+        addstr(stdscr, 9, 0, "Choose an option:")
+        move(stdscr, 10, 0)
         stdscr.clrtoeol()
-        stdscr.addstr(10, 0, "Choose an option:")
-        stdscr.addstr(11, 0, "1. Get Started", colors[0])
-        stdscr.addstr(12, 0, "2. Exit", colors[1])
-        stdscr.move(13, 0)
-        stdscr.clrtoeol()
-        stdscr.addstr(14, 0, ">")
-        stdscr.move(14, 2)
+        addstr(stdscr, 11, 7, "Get Started", colors[0])
+        addstr(stdscr, 11, 21, "Exit", colors[1])
+        footer(
+            stdscr, "Use [◀] and [▶] arrow keys to navigate, and [Enter] to confirm."
+        )
 
-        # stdscr.refresh()
-        stdscr.addstr(14, 1, " " + prompt + " ")
-        stdscr.move(14, 2 + len(prompt))  #* Move cursor to end of prompt
-        ch = stdscr.getch()  #* Wait for user key press
+        ch = getch(stdscr)  #* Wait for user key press
+        if ch == curses.KEY_RESIZE:
+            continue
+
+        if ch == curses.KEY_RIGHT:
+            colors = [curses.color_pair(5), curses.color_pair(7) | curses.A_UNDERLINE]
+        elif ch == curses.KEY_LEFT:
+            colors = [curses.color_pair(7) | curses.A_UNDERLINE, curses.color_pair(5)]
 
         #* If Enter is pressed and requirement is met, exit loop
-        if (ch in (curses.KEY_ENTER, 10, 13)) and (prompt == "1" or prompt == "2"):
+        elif ch in (curses.KEY_ENTER, 10, 13):
             break
 
-        #* Handle backspace
-        elif ch in (curses.KEY_BACKSPACE, 127, 8):
-            colors = [curses.color_pair(5), curses.color_pair(5)]
-            prompt = ""
-
-        #* Accepts only numbers and lowercase letters, so long as the user is allowed to type
-        elif ch == ord("1") or ch == ord("2"):
-            prompt = chr(ch)
-            colors = [curses.color_pair(5), curses.color_pair(5)]
-            colors[int(prompt) - 1] = curses.color_pair(4)
-
     stdscr.clear()
-    stdscr.move(0, 0)
-    # stdscr.refresh()
-    if prompt == "1":
+    curses.curs_set(1)
+    move(stdscr, 0, 0)
+    if colors[0] == curses.color_pair(7) | curses.A_UNDERLINE:
         setup_my_sql(stdscr)
-        
-    elif prompt == "2":
+
+    else:
         #! Exiting...
         pass
 
