b2faf4f5fbe5e5572996431f1f3c700d0d85dd3e
prokenz101 <kenz.v@outlook.com>
2025-07-08 19:03:24 +0400

    moved these functions to curses utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,3 @@
-
 def exception_handler(message="Something went wrong.", Exception=None):
     print("\033[91m\033[1m[Fatal Error]\033[0m")  #* Red color
     print()
@@ -15,34 +14,27 @@ def rgb_to_curses_color(r, g, b):
     return int(r * 1000 / 255), int(g * 1000 / 255), int(b * 1000 / 255)
 
 
-def exit_seal(stdscr):
-    import curses
-    #* Exit the program and restore terminal settings
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-    curses.endwin()
-    exit()
-
-
 def is_all_modules_installed():
     modules = {"windows_curses": False, "cryptography": False, "mysql.connector": False}
     #* Testing if individual modules are installed
 
     try:
         from curses import A_ALTCHARSET
+
         modules["windows_curses"] = True
     except ModuleNotFoundError:
         pass
 
     try:
         from cryptography import __version__
+
         modules["cryptography"] = True
     except ModuleNotFoundError:
         pass
 
     try:
         from mysql.connector import connect
+
         modules["mysql.connector"] = True
     except ModuleNotFoundError:
         pass
@@ -60,8 +52,3 @@ def is_all_modules_installed():
         return False
 
     return True
-
-
-def reset_line(stdscr, y, x):
-    stdscr.move(y, x)
-    stdscr.clrtoeol()
