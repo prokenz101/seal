4b8679904d41c49dceef9239895a01a068f7c09d
prokenz101 <kenz.v@outlook.com>
2025-08-29 16:07:42 +0400

    used conventional "if __name__ == "__main__""

--- main.py
+++ main.py
@@ -17,27 +17,35 @@ def main(stdscr):
     else:
         first_time_launch(stdscr)
 
-    
-try:
-    from scripts.utils import exception_handler, is_all_modules_installed, accounts_exist
 
-    if "idlelib.run" in modules:
-        #! Program runs itself in a terminal if it is run in IDLE
-        #! This is because IDLE does not support curses, or colored text
-        script = path.abspath(__file__) #* Path of main.py
-        system(f'start "" py "{script}"')
+if __name__ == "__main__":
+    try:
+        from scripts.utils import exception_handler, is_all_modules_installed, accounts_exist
 
-    if is_all_modules_installed():
-        import curses
-except ModuleNotFoundError as e:
-    missing_file = str(e).split("'")[1].split(".")[1]
-    print(
-        f"\033[96mMissing required file '\033[92mscripts/{missing_file}.py\033[96m'. Please restore or re-download it.\033[0m"
-    )
-except KeyboardInterrupt:
-    print("\033[91m\033[1mExiting...\033[0m")
-    print("Program interrupted by user.")
-except Exception as e:
-    print("\033[91m\033[1m[Fatal Error]\033[0m")
-    print("Something went wrong.\n")
-    print(e)
+        if "idlelib.run" in modules:
+            #! Program runs itself in a terminal if it is run in IDLE
+            #! This is because IDLE does not support curses, or colored text
+            script = path.abspath(__file__)  #* Path of main.py
+            system(f'start "" py "{script}"')
+
+        if is_all_modules_installed():
+            import curses
+
+            try:
+                curses.wrapper(main)
+            except curses.error:
+                exception_handler("Terminal window is too small to display text.")
+
+    except ModuleNotFoundError as e:
+        #* Handle error if the user deletes necessary script files
+        missing_file = str(e).split("'")[1].split(".")[1]
+        print(
+            f"\033[96mMissing required file '\033[92mscripts/{missing_file}.py\033[96m'. Please restore or re-download it.\033[0m"
+        )
+    except KeyboardInterrupt:
+        print("\033[91m\033[1mExiting...\033[0m")
+        print("Program interrupted by user.")
+    except Exception as e:
+        print("\033[91m\033[1m[Fatal Error]\033[0m")
+        print("Something went wrong.\n")
+        print(e)
